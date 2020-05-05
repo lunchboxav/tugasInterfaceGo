2d382923fd3cf231b0d0fd08d0cf44550379e6ff
Adityo Pratomo <adityo@tetrate.io>
2020-05-05 12:59:35 +0700

    cleanup
    Signed-off-by: Adityo Pratomo <adityo@tetrate.io>

--- model/Interface.go
+++ model/Interface.go
@@ -1,5 +1,6 @@
 package model
 
+// Maju adalah nama interface yang mencakup perhitungan kecepatan dan percetakan data
 type Maju interface {
 	Cepat() float32
 	Lambat() float32
@@ -7,15 +8,3 @@ type Maju interface {
 	CetakMajuCepat()
 	CetakWaktuTempuh()
 }
-
-// func CetakMaju(m Maju) {
-// 	fmt.Println(m)
-// 	v := m.Cepat()
-// 	fmt.Printf("Dengan bergerak cepat, kecepatan %s adalah %.f km/jam\n", v)
-// }
-
-// func CetakWaktuTempuh(m Maju) {
-// 	var jarak float32 = 20
-// 	waktu := m.WaktuTempuh(jarak)
-// 	fmt.Printf("Normalnya, dengan kecepatan %.f km/jam, jarak sejauh %.f km ditempuh selama %.f menit\n", kecepatanSepeda, jarak, waktu)
-// }
